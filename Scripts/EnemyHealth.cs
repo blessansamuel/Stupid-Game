1aea0848ada3eebcf5b2d95082a10c0f123afe26
Blessan <bs5883@suffolkone.ac.uk>
2018-01-07 23:48:42 +0000

    Add files via upload

---
+++ Scripts/EnemyHealth.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyHealth : MonoBehaviour
+{
+	public int startingHealth = 100;            // The amount of health the enemy starts the game with.
+	public int currentHealth;                   // The current health the enemy has.
+	public int scoreValue = 10;                 // The amount added to the player's score when the enemy dies.
+	public AudioClip deathClip;                 // The sound to play when the enemy dies.
+
+
+	Animator anim;                              // Reference to the animator.
+	AudioSource enemyAudio;                     // Reference to the audio source.
+	CapsuleCollider capsuleCollider;            // Reference to the capsule collider.
+	bool isDead;                                // Whether the enemy is dead.
+ 
+
+
+	void Awake ()
+	{
+		// Setting up the references.
+		anim = GetComponent <Animator> ();
+		enemyAudio = GetComponent <AudioSource> ();
+		capsuleCollider = GetComponent <CapsuleCollider> ();
+
+		// Setting the current health when the enemy first spawns.
+		currentHealth = startingHealth;
+	}
+
+
+	public void TakeDamage (int amount, Vector3 hitPoint)
+	{
+		// If the enemy is dead...
+		if(isDead)
+			// ... no need to take damage so exit the function.
+			return;
+
+		// Play the hurt sound effect.
+		enemyAudio.Play ();
+
+		// Reduce the current health by the amount of damage sustained.
+		currentHealth -= amount;
+
+
+		// If the current health is less than or equal to zero...
+		if(currentHealth <= 0)
+		{
+			// ... the enemy is dead.
+			Death ();
+		}
+	}
+
+
+	void Death ()
+	{
+		// The enemy is dead.
+		isDead = true;
+
+		// Turn the collider into a trigger so shots can pass through it.
+		capsuleCollider.isTrigger = true;
+
+		// Tell the animator that the enemy is dead.
+		anim.SetTrigger ("Dead");
+
+		// Change the audio clip of the audio source to the death clip and play it (this will stop the hurt clip playing).
+		enemyAudio.clip = deathClip;
+		enemyAudio.Play ();
+
+		// Increase the score by the enemy's score value.
+		ScoreManager.score += scoreValue;
+	}
+
+
+}
---
+++ Scripts/WaveManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour {
+
+
+	void Awake (){
+
+	}
+
+
+	void Update () {
+
+	}
+
+
+
+}
